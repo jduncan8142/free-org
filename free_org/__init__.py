88e1a7d646e4cf2a293b276f35a27740da020542
Jason Duncan <jason.matthew.duncan@gmail.com>
2025-05-16 09:46:59 -0500

    restart

--- free_org/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Concession Stand Inventory Management System
-
-A Python 3.13 application for tracking inventory for parent organization 
-concession stands at a local high school.
-"""
-
-__version__ = "0.1.0"
